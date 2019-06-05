fafa8b4944ccc7141291679e2949cb3fbfbf7d7e
Your Name <cory.mcshea@gmail.com>
2019-06-05 15:24:52 -0400

    var updates

--- Project1.js
+++ Project1.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
 
 //adding click event listener
     $("#buttons").on("click", function () {
-      var cuisine = $(this).attr("cuisine-type");
-      var location = $(this).attr("locations");
+      var cuisine = $(this).attr("#cuisine-type");
+      var location = $(this).attr("#location");
       var apiKey = "Authorization: Bearer <yCkWYPtYSY8H9cVQ9b6BkhDcpejy-qJFLQcsvLVj2f9zO1Zep2UoYYh1QSn9O7n-2DF8p1tYQS2a0-ajEznSM4kTMrszw9pEW1aV5AGunnhWQXLYDjbHp2tJ_sT1XHYx>"
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cuisine + location + apiKey;
-
+console.log(response)
 
 //  //Api for search parameter
 //  var search = "https://api.yelp.com/v3/businesses/search?q="
