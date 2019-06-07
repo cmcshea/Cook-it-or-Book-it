6ba4ebac5b886583c89169232ba6f1c2d0c77516
cmcshea <cory.mcshea@gmail.com>
2019-06-06 22:25:31 -0400

    api key updated to Zumato

--- Project1.js
+++ Project1.js
@@ -2,23 +2,15 @@
 $(document).ready(function () {
 
 //adding click event listener
-    $("#buttons").on("click", function () {
-      var cuisine = $(this).attr("#cuisine-type");
-      var location = $(this).attr("#location");
-      var apiKey = "Authorization: Bearer <yCkWYPtYSY8H9cVQ9b6BkhDcpejy-qJFLQcsvLVj2f9zO1Zep2UoYYh1QSn9O7n-2DF8p1tYQS2a0-ajEznSM4kTMrszw9pEW1aV5AGunnhWQXLYDjbHp2tJ_sT1XHYx>"
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cuisine + location + apiKey;
-console.log(response)
+    $("#submit-button").on("click", function () {
+    //   var cuisine = $(this).attr("#cuisine-type");
+    //   var location = $(this).attr("#location");
+    //   var cuisine = "chinese"
+    //   var location = "nyc"
+    //curl -X GET --header "Accept: application/json" --header "user-key: 7eab08941dfd95b142f6e1dd717a131b" "https://developers.zomato.com/api/v2.1/categories"
 
-//  //Api for search parameter
-//  var search = "https://api.yelp.com/v3/businesses/search?q="
-
-//  //var inputTerm = ""
-//  //var inputLocation = ""
-//  var key = "Authorization: Bearer <yCkWYPtYSY8H9cVQ9b6BkhDcpejy-qJFLQcsvLVj2f9zO1Zep2UoYYh1QSn9O7n-2DF8p1tYQS2a0-ajEznSM4kTMrszw9pEW1aV5AGunnhWQXLYDjbHp2tJ_sT1XHYx>"
-
-
-// //onclick function for submit
-// $("#submit-button").on("click", function(event) {
+      var apiKey = "&api_key=7eab08941dfd95b142f6e1dd717a131b"
+      var queryURL = "https://developers.zomato.com/api/v2.1/categories" + apiKey;
 
 //prevents page from reloading on form submit
 event.preventDefault();
